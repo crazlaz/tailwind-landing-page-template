7449eb2581182462f2c556d1c4e37f1044adc13c
daGodz <uribae@protonmail.com>
2022-06-06 15:04:49 -0400

    work

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -39,7 +39,7 @@ function HeroHome() {
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Check out our templates! Contact us to get your companys old website a new look!</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="/demo">Get Free Quote</a>
+                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://carlosuribe.vercel.app/demo">Get Free Quote</a>
                 </div>
                 <div>
                   <a href="https://paynani.net" className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4">Main Website</a>
