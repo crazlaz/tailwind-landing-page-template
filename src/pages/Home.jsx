900ecca26e2a088bf566ccb5476f1ba87984dfc3
uribae <43140970+crazlaz@users.noreply.github.com>
2023-02-07 12:05:41 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,133 @@
-import React from 'react';
+import { useState } from 'react'; 
 
-import Header from '../partials/Header';
-import HeroHome from '../partials/HeroHome';
-import FeaturesHome from '../partials/Features';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
-import Testimonials from '../partials/Testimonials';
-import Newsletter from '../partials/Newsletter';
-import Footer from '../partials/Footer';
 
-function Home() {
-  return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-
-      {/*  Site header */}
-      <Header />
-
-      {/*  Page content */}
-      <main className="flex-grow">
-
-        {/*  Page sections */}
-        <HeroHome />
-        <FeaturesHome />
-        <FeaturesBlocks />
-        <Testimonials />
-        <Newsletter />
-
-      </main>
-
-      {/*  Site footer */}
-      <Footer />
-
-    </div>
-  );
+function ProductCategoryRow({ category }) {
+    return (
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
+    )
 }
 
-export default Home;
+function ProductRow({ product }) {
+    const name = product.stocked ? product.name :
+    <span style={{color : 'red'}}>
+        {product.name}
+    </span>;
+
+return (
+    <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+    </tr>
+) }
+
+function ProductTable({ products, filterText, inStockOnly }) {
+    const rows = [];
+    let lastCategory = null;
+  
+    products.forEach((product) => {
+      if (
+        product.name.toLowerCase().indexOf(
+          filterText.toLowerCase()
+        ) === -1
+      ) {
+        return;
+      }
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category} />
+        );
+      }
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name} />
+      );
+      lastCategory = product.category;
+    });
+  
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+
+function SearchBar(
+    {
+        filterText,
+        inStockOnly,
+        onFilterTextChange,
+        onInStockOnlyChange
+    }
+) {
+    return (
+        <form>
+
+            <input type="text" placeholder="Search...." 
+            onChange={(e) => onFilterTextChange(e.target.value)}
+            />
+            <label>
+                <input type="checkbox"
+                checked={inStockOnly}
+                onChange={(e) => onInStockOnlyChange(e.target.checked)}
+                />
+                {' '}
+                Only show products in stock 
+            </label>
+        </form>
+    )
+}
+
+function FilterableProductTable({products }) {
+
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+
+
+    return (
+        <div>
+            <SearchBar 
+            filterText={filterText}
+            inStockOnly={inStockOnly}
+            onFilterTextChange={setFilterText}
+            onInStockOnlyChange={setInStockOnly} 
+            />
+            <ProductTable 
+                products={products} 
+                filterText={filterText}
+                inStockOnly={inStockOnly} />
+            
+        </div>
+    )
+}
+
+
+const PRODUCTS = [
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"},
+    {category: "Vegetables", price: "$5", stocked: true, name: "Tomato"}
+]
+
+
+export default function App() {
+    return <FilterableProductTable products={PRODUCTS} />;
+}
